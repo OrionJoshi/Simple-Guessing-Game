425a56779c43b02b92084f40528ac302851fa17e
khageshwor <khageshwarjoshi03@gmail.com>
2020-02-04 15:38:51 -0800

    Main file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP Guessing Game</title>
+</head>
+<body>
+
+      <form action="gen.php" method="POST">
+        
+        Guess: <input type="number" name="guess"><br>
+               <input type="submit" name="submit" value="Guess">
+
+
+      </form>
+
+</body>
+</html>
